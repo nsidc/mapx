bc15f169e7ffafd6a7bd62b078231baa6beb0718
Ken Knowles <programmers@nsidc.org>
1992-12-30 14:18:11 +0000

    added interactive and performance monitoring tests

--- grids.c
+++ grids.c
@@ -4,8 +4,11 @@
  *	26-Dec-1991 K.Knowles knowles@kryos.colorado.edu 303-492-0644
  *	7-July-1992 K.Knowles - changed grid_struct to grid_class
  *				updated references to mapx
+ *	30-Dec-1992 K.Knowles - added interactive and performance tests
  *========================================================================*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <define.h>
 #include <mapx.h>
 #include <grids.h>
@@ -133,9 +136,12 @@ void close_grid(grid_class *this)
  *------------------------------------------------------------------------*/
 int forward_grid(grid_class *this, float lat, float lon, float *r, float *s)
 {
+  register int status;
   float u,v;
 
-  forward_mapx(this->mapx, lat, lon, &u, &v);
+  status = forward_mapx(this->mapx, lat, lon, &u, &v);
+  if (status != 0) return FALSE;
+
   *r = this->map_origin_col + u * this->cols_per_map_unit;
   *s = this->map_origin_row - v * this->rows_per_map_unit;
 
@@ -159,10 +165,102 @@ int forward_grid(grid_class *this, float lat, float lon, float *r, float *s)
  *------------------------------------------------------------------------*/
 int inverse_grid (grid_class *this, float r, float s, float *lat, float *lon)
 {
+  register int status;
   float u,v;
 
   u =  (r - this->map_origin_col) / this->cols_per_map_unit;
   v = -(s - this->map_origin_row) / this->rows_per_map_unit;
-  inverse_mapx(this->mapx, u, v, lat, lon);
+  status = inverse_mapx(this->mapx, u, v, lat, lon);
+  if (status != 0) return FALSE;
   return within_mapx(this->mapx, *lat, *lon);
 }
+
+#ifdef GTEST
+/*========================================================================
+ * gtest - interactive test grid routines
+ *========================================================================*/
+main(int argc, char* argv[])
+{
+  float lat, lon, r, s;
+  int status;
+  char readln[80];
+  grid_class *the_grid;
+
+  for (;;)
+  { printf("\nenter .gpd file name - ");
+    gets(readln);
+    if (feof(stdin)) { printf("\n"); exit(0);}
+    if (*readln == '\0') break;
+    the_grid = init_grid(readln);
+    if (the_grid == NULL) continue;
+
+    printf("\nforward_grid:\n");
+    for (;;)
+    { printf("enter lat lon - ");
+      gets(readln);
+      if (feof(stdin)) { printf("\n"); exit(0);}
+      if (*readln == '\0') break;
+      sscanf(readln, "%f %f", &lat, &lon);
+      status = forward_grid(the_grid, lat, lon, &r, &s);
+      printf("col,row = %f %f    status = %d\n", r, s, status);
+      status = inverse_grid(the_grid, r, s, &lat, &lon);
+      printf("lat,lon = %f %f    status = %d\n", lat, lon, status);
+    }
+
+    printf("\ninverse_grid:\n");
+    for (;;)
+    { printf("enter r s - ");
+      gets(readln);
+      if (feof(stdin)) { printf("\n"); exit(0);}
+      if (*readln == '\0') break;
+      sscanf(readln, "%f %f", &r, &s);
+      status = inverse_grid(the_grid, r, s, &lat, &lon);
+      printf("lat,lon = %f %f    status = %d\n", lat, lon, status);
+      status = forward_grid(the_grid, lat, lon, &r, &s);
+      printf("col,row = %f %f    status = %d\n", r, s, status);
+    }
+  }
+}
+#endif
+
+#ifdef GPMON
+/*========================================================================
+ * gpmon - performance test grid routines
+ *========================================================================*/
+#define usage "usage: gpmon gpd_file [num_its]"
+main(int argc, char* argv[])
+{
+  register int ii, npts = 0, status;
+  int its = 1;
+  register float r, s;
+  float lat, lon, rx, sx;
+  grid_class *the_grid;
+
+  if (argc < 2) 
+  { fprintf(stderr,"#\tgpmon can be used to monitor the performance\n");
+    fprintf(stderr,"#\tof the grid routines. It runs the forward and\n");
+    fprintf(stderr,"#\tinverse transforms on each point in the grid.\n");
+    fprintf(stderr,"#\tThe optional parameter num_its specifies how\n");
+    fprintf(stderr,"#\tmany times to run through the entire grid, (the\n");
+    fprintf(stderr,"#\tdefault is 1). To run the test type:\n");
+    fprintf(stderr,"#\t\tgpmon test.gpd\n");
+    fprintf(stderr,"#\t\tprof gpmon\n");
+    fprintf(stderr,"\n");
+    error_exit(usage);
+  }
+  the_grid = init_grid(argv[1]);
+  if (the_grid == NULL) error_exit(usage);
+  if (argc < 3 || sscanf(argv[2],"%d",&its) != 1) its = 1;
+
+  for (ii = 1; ii <= its; ii++)
+  { for (r = 0; r < the_grid->cols; r++)
+    { for (s = 0; s < the_grid->rows; s++)
+      { ++npts;
+	status = inverse_grid(the_grid, r, s, &lat, &lon);
+	status = forward_grid(the_grid, lat, lon, &rx, &sx);
+      }
+    }
+  }
+  fprintf(stderr,"%d points\n", npts);
+}
+#endif
