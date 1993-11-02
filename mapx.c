644866b0f665ae4ccc8944cf4c8dc2cba59a4224
Ross Swick <swick@nsidc.org>
1993-11-02 12:06:59 +0000

    Added polar stereographic ellipsoid projection

--- mapx.c
+++ mapx.c
@@ -4,7 +4,7 @@
  * 2-July-1991 K.Knowles knowles@kryos.colorado.edu 303-492-0644
  * 10-Dec-1992 R.Swick swick@krusty.colorado.edu 303-492-1395
  *========================================================================*/
-static const char mapx_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/mapx.c,v 1.15 1993-10-27 16:53:10 knowles Exp $";
+static const char mapx_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/mapx.c,v 1.16 1993-11-02 12:06:59 swick Exp $";
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +24,7 @@ static mapx_class *current;
 /*
  * function prototypes for map transformations
  */
-static int init_azimuthal_equal_area(void); /* one for each projection */
+static int init_azimuthal_equal_area(void);                              /* one for each projection */
 static int azimuthal_equal_area(float,float,float*,float*);
 static int inverse_azimuthal_equal_area(float,float,float*,float*);
 static int init_cylindrical_equidistant(void);
@@ -502,7 +502,7 @@ int forward_mapx (mapx_class *this, float lat, float lon, float *u, float *v)
 /*------------------------------------------------------------------------
  * inverse_mapx - inverse map transformation
  *
- *	input : this - pointer to map data structure (returned byinit_mapx)
+ *	input : this - pointer to map data structure (returned by init_mapx)
  *		u,v - map coordinates in map units
  *
  *	output: lat,lon - geographic coordinates in decimal degrees
@@ -560,13 +560,13 @@ static char *standard_name(char *s)
 	   (streq(new_name, "EQUIDISTANTCYLINDRICAL") != NULL) )
   { strcpy(new_name, "CYLINDRICALEQUIDISTANT");
   }
-  else if ((streq(new_name, "POLARSTEREOGRAPHIC") != NULL)|| 
+  else if ((streq(new_name, "POLARSTEREOGRAPHIC") != NULL) || 
 	   (streq(new_name, "STEREOGRAPHICPOLAR") != NULL))
   { strcpy(new_name, "POLARSTEREOGRAPHIC");
   }
-  else if ((streq(new_name, "POLARSTEREOGRAPHICELLIPSOID") != NULL)|| 
-	   (streq(new_name, "ELLIPSOIDPOLARSTEREOGRAPHIC") != NULL))
-	   (streq(new_name, "STEREOGRAPHICPOLARELLIPSOID") != NULL))
+  else if ((streq(new_name, "POLARSTEREOGRAPHICELLIPSOID") != NULL) || 
+	   (streq(new_name, "ELLIPSOIDPOLARSTEREOGRAPHIC") != NULL) ||
+	   (streq(new_name, "STEREOGRAPHICPOLARELLIPSOID") != NULL) ||
 	   (streq(new_name, "ELLIPSOIDSTEREOGRAPHICPOLAR") != NULL))
   { strcpy(new_name, "POLARSTEREOGRAPHICELLIPSOID");
   }
@@ -807,8 +807,9 @@ static int inverse_azimuthal_equal_area_ellipsoid (float u, float v, float *lat,
   double phi, lam, rho, x, y, ce, beta;
   
   if (current->eccentricity == 0.0)
-  {inverse_azimuthal_equal_area (u, v, lat, lon);
- }
+  {
+    inverse_azimuthal_equal_area (u, v, lat, lon);
+  }
   else
   {
     x = current->T00 * (u + current->u0) - (current->T01) * (v + current->v0);
@@ -926,13 +927,13 @@ static int polar_stereographic (float lat, float lon, float *u, float *v)
   phi = RADIANS (lat);
   lam = RADIANS (lon - current->lon0);
   
-  if (current->lat0 == 90)
+  if (90.0 == current->lat0)
   { rho = current->Rg * cos(phi) 
       * (1 + current->sin_phi1) / (1 + sin(phi));
     x =  rho * sin(lam);
     y = -rho * cos(lam);
   }
-  else if (current->lat0 == -90)
+  else if (-90.0 == current->lat0)
   { rho = current->Rg * cos(phi) 
       * (1 - current->sin_phi1) / (1 - sin(phi));
     x = rho * sin(lam);
@@ -954,13 +955,13 @@ static int inverse_polar_stereographic (float u, float v, float *lat, float *lon
   
   rho = sqrt(x*x + y*y);
   q = current->Rg*(1 + current->sin_phi1);
-  c = 2*atan2(rho,q);
+  c = 2*atan2(rho, q);
   
-  if (current->lat0 == 90)
+  if (90.0 == current->lat0)
   { phi = asin(cos(c));
     lam = atan2(x, -y);
   }
-  else if (current->lat0 == -90)
+  else if (-90.0 == current->lat0)
   { phi = asin(-cos(c));
     lam = atan2(x, y);
   }
@@ -978,13 +979,48 @@ static int inverse_polar_stereographic (float u, float v, float *lat, float *lon
 
 static int init_polar_stereographic_ellipsoid(void)
 {
+  float numerator, denominator;
+
   if (current->lat1 == 999) current->lat1 = current->lat0;
-  current->sin_phi1 = sin (RADIANS (current->lat1));
   if (current->lat0 != 90.00 && current->lat0 != -90.00)
   { fprintf(stderr,"mapx: only polar aspects allowed: lat0 = %7.2f\n",
 	    current->lat0);
     return -1;
+  } 
+  current->Rg = current->equatorial_radius / current->scale;
+  
+  if(90.0 == current->lat0)
+  {
+    current->cos_phi1 = cos(RADIANS(current->lat1));
+    current->sin_phi0 = sin(RADIANS(current->lat0));
+    current->sin_phi1 = sin(RADIANS(current->lat1));
   }
+
+  else if(-90.0 == current->lat0)     
+  {
+    current->cos_phi1 = cos(RADIANS(-current->lat1));
+    current->sin_phi0 = sin(RADIANS(-current->lat0));
+    current->sin_phi1 = sin(RADIANS(-current->lat1));
+  }
+
+  current->m1 = ((current->cos_phi1) / sqrt(1 - (current->e2 * current->sin_phi1 
+					     * current->sin_phi1)));
+  
+  numerator = (1 - current->eccentricity * current->sin_phi1);
+  denominator =  (1 + current->eccentricity * current->sin_phi1);
+   
+  if(90.0 == current->lat0)
+  {
+    current->t1 = tan(PI / 4 - RADIANS(current->lat1) / 2) 
+      / pow(numerator / denominator, current->eccentricity / 2);
+  }
+  
+  else if(-90.0 == current->lat0)
+  {
+    current->t1 = tan(PI / 4 - RADIANS(-current->lat1) / 2) 
+      / pow(numerator / denominator, current->eccentricity / 2);
+  }
+  
   return 0;
 }
 
@@ -992,24 +1028,46 @@ static int polar_stereographic_ellipsoid(float lat, float lon,
 					 float *u, float *v)
 {
   float x, y;
-  float phi, lam, rho;
-  
-  phi = RADIANS (lat);
-  lam = RADIANS (lon - current->lon0);
-  
-  if (current->lat0 == 90)
-  { rho = current->Rg * cos(phi) 
-      * (1 + current->sin_phi1) / (1 + sin(phi));
-    x =  rho * sin(lam);
-    y = -rho * cos(lam);
+  float phi, lam, rho, t, m1, temp1, temp2;
+  float numerator, denominator;
+    
+  if (90.0 == current->lat0)
+  {  
+    phi = RADIANS(lat);
+    lam = RADIANS(lon - current->lon0);
   }
-  else if (current->lat0 == -90)
-  { rho = current->Rg * cos(phi) 
-      * (1 - current->sin_phi1) / (1 - sin(phi));
-    x = rho * sin(lam);
-    y = rho * cos(lam);
+  else if(-90.0 == current->lat0)
+  {  
+    phi = RADIANS(-lat);
+    lam = RADIANS(-lon + current->lon0);
   }
-  
+
+  numerator = (1 - current->eccentricity * sin(phi));
+  denominator =  (1 + current->eccentricity * sin(phi));
+  t = tan(PI / 4 - phi / 2) 
+    / pow(numerator / denominator, current->eccentricity / 2);
+
+  if((90.0 != current->lat1) && (-90.0 != current->lat1))
+  {
+    rho = current->Rg * current->m1 * t / current->t1; 
+  }
+  else 
+  { 
+    numerator = 2 * current->Rg * current->scale * t;
+    denominator = sqrt(pow(1 + current->eccentricity, 1 + current->eccentricity)
+		       * pow(1 - current->eccentricity, 1 - current->eccentricity));
+    rho = numerator / denominator;
+  }
+
+  x =  rho * sin(lam);
+  y = -rho * cos(lam);
+
+  if(-90.0 == current->lat0)
+  {
+    x = -x;
+    y = -y;
+  }
+
   *u = current->T00*x + current->T01*y - current->u0;
   *v = current->T10*x + current->T11*y - current->v0;
   
@@ -1019,28 +1077,59 @@ static int polar_stereographic_ellipsoid(float lat, float lon,
 static int inverse_polar_stereographic_ellipsoid(float u, float v, 
 						 float *lat, float *lon)
 {
-  double phi, lam, rho, c, q, x, y;
-  
+  double phi, lam, rho, chi, t, x, y;
+  double numerator, denominator;
+  double sin2chi, sin4chi, sin6chi;
+
   x =  current->T00*(u+current->u0) - current->T01*(v + current->v0);
   y = -current->T10*(u+current->u0) + current->T11*(v+current->v0);
   
   rho = sqrt(x*x + y*y);
-  q = current->Rg*(1 + current->sin_phi1);
-  c = 2*atan2(rho,q);
-  
-  if (current->lat0 == 90)
-  { phi = asin(cos(c));
+
+  if(90.0 == current->lat1 || -90.0 == current->lat1)
+  {
+    numerator = rho * sqrt(pow(1 + current->eccentricity, 1 + current->eccentricity) 
+			   * pow(1 - current->eccentricity, 1 - current->eccentricity) );
+    denominator = 2 * current->Rg * current->scale;
+    t = numerator / denominator;
+  }
+  else
+  {
+    numerator = rho * current->t1;
+    denominator = current->Rg * current->m1;
+    t = numerator / denominator;
+  }
+ 
+
+  chi = PI / 2.0 - 2.0 * atan(t);
+  sin2chi = sin(2.0 * chi);
+  sin4chi = sin(4.0 * chi);
+  sin6chi = sin(6.0 * chi);
+
+  phi = chi + (sin2chi * current->e2 / 2.0) + (sin2chi * 5.0 * current->e4 / 24.0)
+    + (sin2chi * current->e6 / 12.0) + (sin2chi * 13.0 * current->e8 / 360.0) 
+      + (sin4chi * 7.0 * current->e4 / 48.0) + (sin4chi * 29.0 * current->e6 / 240.0)
+	+ (sin4chi * 811.0 * current->e8 / 11520.0)
+	  + (sin6chi * 7.0 * current->e6 / 120.0)
+	    +(sin6chi * 81.0 * current->e8 / 1120.0)
+	      +(sin(8.0 * chi) * 4279.0 * current->e8 / 161280.0);
+     
+  if(90.0 == current->lat0)
+  {  
+    *lat = DEGREES(phi);
+    
     lam = atan2(x, -y);
+    *lon = DEGREES(lam) + current->lon0;
+    NORMALIZE(*lon);
   }
-  else if (current->lat0 == -90)
-  { phi = asin(-cos(c));
-    lam = atan2(x, y);
-  }
-  
-  *lat = DEGREES(phi);
-  *lon = DEGREES(lam) + current->lon0;
-  NORMALIZE(*lon);
-  
+  else if(-90.0 == current->lat0)
+  {  
+    *lat = -DEGREES(phi);
+    
+    lam = atan2(-x, y);
+    *lon  = -DEGREES(lam) + current->lon0;
+    NORMALIZE(*lon);
+  } 
   return 0;
 }
 
@@ -1468,9 +1557,9 @@ static int init_lambert_conic_conformal_ellipsoid(void)
   current->cos_phi1 = cos(RADIANS(current->lat1));
   current->sin_phi0 = sin(RADIANS(current->lat0));
   current->sin_phi1 = sin(RADIANS(current->lat1));
-  current->m0 = ((current->cos_phi0)/sqrt(1-(current->e2 * current->sin_phi0 
+  current->m0 = ((current->cos_phi0)/sqrt(1 - (current->e2 * current->sin_phi0 
 					     * current->sin_phi0))); 
-  current->m1 = ((current->cos_phi1)/sqrt(1-(current->e2 * current->sin_phi1 
+  current->m1 = ((current->cos_phi1)/sqrt(1 - (current->e2 * current->sin_phi1 
 					     * current->sin_phi1)));
   current->t0 = sqrt( ((1.0 - current->sin_phi0)/(1.0 + current->sin_phi0)) * 
 		     pow(((1.0 + (current->eccentricity * current->sin_phi0))/
@@ -1523,7 +1612,7 @@ static int inverse_lambert_conic_conformal_ellipsoid (float u, float v, float *l
   rho = (fabs(current->n) / current->n) * 
     sqrt((x*x) + ((current->rho0 - y) * (current->rho0 - y)));
   t = pow((rho/(current->Rg * current->F)), (1/current->n));
-  chi = ((PI/2.0) - (2.0 * atan(t)));
+  chi = PI/2.0 - 2.0 * atan(t);
   
   if (current->n < 0.0)
     theta = atan( -x / (y - current->rho0));
@@ -1625,7 +1714,7 @@ static float dist_km(float lat1, float lon1, float lat2, float lon2)
   phi2 = radians(lat2);
   lam2 = radians(lon2);
   beta = acos(cos(phi1)*cos(phi2)*cos(lam1-lam2) + sin(phi1)*sin(phi2));
-  return beta*Re;
+  return beta*mapx_Re_km;
 }
 #endif
 
@@ -1659,7 +1748,7 @@ main(int argc, char *argv[])
     fprintf(stderr,"\n");
     error_exit(usage);
   }
-  else
+#else
   { fprintf(stderr,"#\tmpmon can be used to monitor the performance\n");
     fprintf(stderr,"#\tof the mapx routines. It runs the forward and\n");
     fprintf(stderr,"#\tinverse transforms at ~100K points over the whole\n");
