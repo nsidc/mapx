c6a3d9901c5b1fc20df1b47379969d40422bde64
Ken Knowles <programmers@nsidc.org>
1993-11-03 16:43:08 +0000

    *** empty log message ***

--- matrix.h
+++ matrix.h
@@ -6,8 +6,8 @@
 #ifndef matrix_h_
 #define matrix_h_
 
-static const char matrix_h_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/matrix.h,v 1.1 1993-08-02 10:20:47 knowles Exp $";
+static const char matrix_h_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/matrix.h,v 1.2 1993-11-03 16:43:08 knowles Exp $";
 
-void **matrix (int rows, int cols, int bytes, int zero);
+void **matrix(int rows, int cols, int bytes, int zero);
 
 #endif
