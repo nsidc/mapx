2a12488fa60aacc7a8d0f21e0233da21737e2ad5
Ken Knowles <programmers@nsidc.org>
1993-11-03 15:27:30 +0000

    changed function names, Polynomials allocate themselves now got rid of MAXVARS and MAXPOINTS

--- pmodel.h
+++ pmodel.h
@@ -1,36 +1,33 @@
 /*======================================================================
- * pmodel - approximation by polynomial model
+ * pmodel - polynomial model
  *
  * 2-Aug-1990 K.Knowles knowles@sastrugi.colorado.edu 303-492-0644
  *======================================================================*/
 #ifndef pmodel_h_
 #define pmodel_h_
 
-static const char pmodel_h_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/pmodel.h,v 1.2 1993-08-02 10:30:26 knowles Exp $";
+static const char pmodel_h_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/pmodel.h,v 1.3 1993-11-03 15:27:30 knowles Exp $";
 
-#define MAXVARS 64
-#define MAXPOINTS 450
+#define ipow(x,i) \
+((i) == 0 ? 1.0 : (i) == 1 ? (x) : (x) == 0.0 ? 0.0 : pow((x), (double) (i)))
 
-#define ipow(x,i) ((i) == 0 ? 1.0 : (x) == 0.0 ? 0.0 : pow((x), (double) (i)))
+typedef struct 
+{ int dim;	/* dimension	*/
+  int order;	/* size		*/
+  int tcode;	/* shape	*/
+  double *coef;	/* coefficients */
+} Polynomial;
 
-typedef struct {
-	int dim;		/* dimension	*/
-	int order;		/* size		*/
-	int tcode;		/* shape	*/
-	double coef[MAXVARS];	/* coefficients */
-	} Polynomial;
+Polynomial *init_pmodel(int dim, int order, int tcode, int npts,
+                        double *rdata, double *sdata, double *tdata);
 
-double peval (Polynomial *P, double r, double s);
+void free_pmodel(Polynomial *P);
 
-void pmatrix (Polynomial *P, int npts, double M[MAXPOINTS][MAXVARS],
-	      double r_data[MAXPOINTS], double s_data[MAXPOINTS]);
+double eval_pmodel(Polynomial *P, double r, double s);
 
-void pmodel (Polynomial *P, int npts, double M[MAXPOINTS][MAXVARS],
-	     double t_data[MAXPOINTS], char *method);
-
-void pstats (double *SSE, double *R2, Polynomial *P, int npts, 
-	     double t_data[MAXPOINTS], double r_data[MAXPOINTS], 
-	     double s_data[MAXPOINTS]);
+void test_pmodel(Polynomial *P, int npts, 
+		 double *rdata, double *sdata, double *tdata,
+		 double *SSE, double *R2);
 
 double chebyshev(int i, int n, double a, double b);
 
