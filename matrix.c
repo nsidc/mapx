04e92614dc33292fa0b6695cef8766377881f333
Ken Knowles <programmers@nsidc.org>
1996-03-07 23:19:40 +0000

    fixed a bug where a matrix of doubles with an odd number of rows was misaligned and caused a Bus Error it should now be possible to align any size matrix element also made the zero flag functional again

--- matrix.c
+++ matrix.c
@@ -6,10 +6,12 @@
  *========================================================================*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "define.h"
 #include "matrix.h"
 
-static const char matrix_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/matrix.c,v 1.6 1996-02-29 21:59:11 knowles Exp $";
+static const char matrix_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/matrix.c,v 1.7 1996-03-07 23:19:40 knowles Exp $";
 
 /*------------------------------------------------------------------------
  * matrix - allocate 2-D matrix
@@ -20,30 +22,49 @@ static const char matrix_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/matrix.c,v 1.
  *		zero - if set then zero fill entries
  *
  *	result: pointer to column of pointers to rows
+ *		i.e. matrix is accessed as matrix_ptr[row][col]
+ *		data storage block is contiguous starting at matrix_ptr[0]
  *
- *	note: resources can be de-allocated using the normal free routine
- *		assumes all pointers are the same size, sizeof(void *)
- *		currently ignores zero flag (i.e. always clears block)
+ *	note: Resources can be de-allocated using the normal free routine.
+ *	      Matrix entries will be suitably aligned for all primitive 
+ *	      types. It is up to the caller to ensure that structures are
+ *	      properly aligned, i.e. bytes should be a multiple of the
+ *	      sizeof the first member of the struct. 
  *
- *----------------------------------------------------------------------*/
+ *------------------------------------------------------------------------*/
 void **matrix(int rows, int cols, int bytes, int zero)
 {
-  void **matrix_ptr, **row_ptr;
-  char *block;
+  register int irow;
+  void **matrix_ptr;
+  char *block_ptr, **row_ptr;
   size_t row_size, row_ptr_size;
-  register int irow, free_byte;
 
+/*
+ *	allocate row pointer storage followed by data storage
+ *	make sure the data storage is suitably alligned
+ */
   row_size = cols*bytes;
   row_ptr_size = rows * sizeof(void *);
-  block = (char *)calloc(row_ptr_size + rows*row_size, sizeof(char));
-  if (block == NULL) { perror("matrix"); return(NULL); }
-  matrix_ptr = (void **) block;
-  row_ptr = matrix_ptr;
-  free_byte = row_ptr_size;
+  row_ptr_size = ceil((double)row_ptr_size/bytes) * bytes;
+  block_ptr = (char *)malloc(row_ptr_size + rows*row_size);
+  if (NULL == block_ptr) { perror("matrix"); return(NULL); }
+
+/*
+ *	assign row pointers
+ */
+  matrix_ptr = (void **)block_ptr;
+  row_ptr = (char **)matrix_ptr;
+  block_ptr += row_ptr_size;
   for (irow = 0; irow < rows; irow++)
-  { *row_ptr = &(block[free_byte]);
-    free_byte += row_size;
+  { *row_ptr = block_ptr;
+    block_ptr += row_size;
     ++row_ptr;
   }
+
+/*
+ *	clear data area
+ */
+  if (zero) memset(matrix_ptr[0], 0, rows*row_size);
+
   return(matrix_ptr);
 }
