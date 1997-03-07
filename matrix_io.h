b7d2738708c89afd583d276fdb5093b1680258e8
Mary Jo Brodzik <brodzik@nsidc.org>
1997-03-07 19:42:09 +0000

    Took _data off names.  Reran unit test.

--- matrix_io.h
+++ matrix_io.h
@@ -2,15 +2,27 @@
  * matrix_io.h - header file for matrix_io.c:
  *   programs to read and write matrix data
  * 
+ * 03/7/1997 brodzik brodzik@zamboni.colorado.edu 303-492-8263
+ *
+ * National Snow & Ice Data Center, University of Colorado, Boulder
+ *
  *$Log: not supported by cvs2svn $
+ * Revision 1.1  1997/02/27  21:55:27  brodzik
+ * Initial revision
+ *
  *
  *======================================================================*/
+#ifndef matrix_io_h_
+#define matrix_io_h_
+
 #include <stdio.h>
 #include <define.h>
 
-static const char matrix_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/matrix_io.h,v 1.1 1997-02-27 21:55:27 brodzik Exp $";
+static const char matrix_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/matrix_io.h,v 1.2 1997-03-07 19:42:09 brodzik Exp $";
 
-size_t read_matrix_data (void **data, char *file_name, 
-			 int rows, int cols, size_t size);
-size_t write_matrix_data (char *file_name, void **at, 
-			  int rows, int cols, size_t size);
+size_t read_matrix (void **data, char *file_name, 
+		    int rows, int cols, size_t size);
+size_t write_matrix (char *file_name, void **at, 
+		     int rows, int cols, size_t size);
+
+#endif
