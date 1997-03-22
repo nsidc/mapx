89ea97133b6b2ef18f8a6d79b4b675b5a9bc1823
Mary Jo Brodzik <brodzik@nsidc.org>
1997-03-22 22:54:20 +0000

    Changed obsolete **at to **data.

--- matrix_io.h
+++ matrix_io.h
@@ -7,6 +7,9 @@
  * National Snow & Ice Data Center, University of Colorado, Boulder
  *
  *$Log: not supported by cvs2svn $
+ * Revision 1.2  1997/03/07  19:42:09  brodzik
+ * Took _data off names.  Reran unit test.
+ *
  * Revision 1.1  1997/02/27  21:55:27  brodzik
  * Initial revision
  *
@@ -15,14 +18,13 @@
 #ifndef matrix_io_h_
 #define matrix_io_h_
 
-#include <stdio.h>
 #include <define.h>
 
-static const char matrix_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/matrix_io.h,v 1.2 1997-03-07 19:42:09 brodzik Exp $";
+static const char matrix_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/matrix_io.h,v 1.3 1997-03-22 22:54:20 brodzik Exp $";
 
 size_t read_matrix (void **data, char *file_name, 
 		    int rows, int cols, size_t size);
-size_t write_matrix (char *file_name, void **at, 
+size_t write_matrix (char *file_name, void **data, 
 		     int rows, int cols, size_t size);
 
 #endif
