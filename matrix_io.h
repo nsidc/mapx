32aae2fc5eaf6e4eac0c94fd9be626fda2f655ad
Mary Jo Brodzik <brodzik@nsidc.org>
1997-09-26 17:37:10 +0000

    Added initialize_matrix function.

--- matrix_io.h
+++ matrix_io.h
@@ -7,6 +7,9 @@
  * National Snow & Ice Data Center, University of Colorado, Boulder
  *
  *$Log: not supported by cvs2svn $
+ * Revision 1.3  1997/03/22  22:54:20  brodzik
+ * Changed obsolete **at to **data.
+ *
  * Revision 1.2  1997/03/07  19:42:09  brodzik
  * Took _data off names.  Reran unit test.
  *
@@ -19,12 +22,20 @@
 #define matrix_io_h_
 
 #include <define.h>
+#include "grids.h"
 
-static const char matrix_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/matrix_io.h,v 1.3 1997-03-22 22:54:20 brodzik Exp $";
+static const char matrix_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/matrix_io.h,v 1.4 1997-09-26 17:37:10 brodzik Exp $";
 
 size_t read_matrix (void **data, char *file_name, 
 		    int rows, int cols, size_t size);
 size_t write_matrix (char *file_name, void **data, 
 		     int rows, int cols, size_t size);
+
+/* Allocate memory for and read matrix object from external file */
+void **initialize_matrix (grid_class *grid, 
+			  size_t size, 
+			  const char *file_name,
+			  const char *object_name, 
+			  bool verbose);
 
 #endif
