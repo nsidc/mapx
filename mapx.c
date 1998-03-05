55597d16ef9e90b1a6f3ce6d2f11e48c77615d0a
Ken Knowles <programmers@nsidc.org>
1998-03-05 00:00:46 +0000

    added interupted homolosine equal-area

--- mapx.c
+++ mapx.c
@@ -5,7 +5,7 @@
  * 10-Dec-1992 R.Swick swick@krusty.colorado.edu 303-492-1395
  * National Snow & Ice Data Center, University of Colorado, Boulder
  *========================================================================*/
-static const char mapx_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/mapx.c,v 1.24 1998-02-26 17:58:21 swick Exp $";
+static const char mapx_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/mapx.c,v 1.25 1998-03-05 00:00:46 knowles Exp $";
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -61,6 +61,9 @@ static int inverse_cylindrical_equal_area_ellipsoid(float,float,float*,float*);
 static int init_lambert_conic_conformal_ellipsoid(void);
 static int lambert_conic_conformal_ellipsoid(float,float,float*,float*);
 static int inverse_lambert_conic_conformal_ellipsoid(float,float,float*,float*);
+static int init_interupted_homolosine_equal_area(void);
+static int interupted_homolosine_equal_area(float,float,float*,float*);
+static int inverse_interupted_homolosine_equal_area(float,float,float*,float*);
 static char *standard_name(char *);
 
 /*----------------------------------------------------------------------
@@ -95,6 +98,7 @@ static char *standard_name(char *);
  *			 Azimuthal_Equal_Area_Ellipsoid			 
  *			 Cylindrical_Equal_Area_Ellipsoid
  *                       Lambert_Conic_Conformal_Ellipsoid
+ *			 Interupted_Homolosine_Equal_Area
  *			or anything reasonably similar
  *
  *	The parameter lat1 is currently used for the Polar_Stereographic
@@ -317,6 +321,11 @@ mapx_class *init_mapx (char *map_filename)
     this->geo_to_map = lambert_conic_conformal_ellipsoid;
     this->map_to_geo = inverse_lambert_conic_conformal_ellipsoid;
   }
+  else if (strcmp (projection, "INTERUPTEDHOMOLOSINEEQUALAREA") == 0) 
+  { this->initialize =init_interupted_homolosine_equal_area;
+    this->geo_to_map = interupted_homolosine_equal_area;
+    this->map_to_geo = inverse_interupted_homolosine_equal_area;
+  }
   else
   { fprintf (stderr, "init_mapx: unknown projection %s\n", original_name);
     fprintf (stderr, "valid types are:\n");
@@ -325,6 +334,7 @@ mapx_class *init_mapx (char *map_filename)
     fprintf (stderr, " Cylindrical Equal-Area\n");
     fprintf (stderr, " Cylindrical Equal-Area Ellipsoid\n");
     fprintf (stderr, " Cylindrical Equidistant\n");
+    fprintf (stderr, " Interupted Homolosine Equal-Area\n");
     fprintf (stderr, " Lambert Conic Conformal Ellipsoid\n");
     fprintf (stderr, " Mercator\n");
     fprintf (stderr, " Mollweide\n");
@@ -588,6 +598,16 @@ static char *standard_name(char *s)
 	   streq(new_name, "ELLIPSOIDLAMBERTCONICCONFORMAL") ||
 	   streq(new_name, "ELLIPSOIDLAMBERTCONFORMALCONIC"))
   { strcpy(new_name, "LAMBERTCONICCONFORMALELLIPSOID");
+  }
+  else if (streq(new_name, "INTERUPTEDHOMOLOSINEEQUALAREA") ||
+	   streq(new_name, "GOODESINTERUPTEDHOMOLOSINE") ||
+	   streq(new_name, "GOODEHOMOLOSINEEQUALAREA") ||
+	   streq(new_name, "GOODESHOMOLOSINEEQUALAREA") ||
+	   streq(new_name, "INTERUPTEDHOMOLOSINE") ||
+	   streq(new_name, "GOODEINTERUPTEDHOMOLOSINE") ||
+	   streq(new_name, "GOODEHOMOLOSINE") ||
+	   streq(new_name, "GOODESHOMOLOSINE"))
+  { strcpy(new_name, "INTERUPTEDHOMOLOSINEEQUALAREA");
   }
   
   return new_name;
@@ -1663,6 +1683,232 @@ static int inverse_lambert_conic_conformal_ellipsoid (float u, float v, float *l
   
   return 0;
 }
+/*--------------------------------------------------------------------------
+ * interupted homolosine equal-area
+ *--------------------------------------------------------------------------*/
+
+/* iteration control */
+#define IH_MAXITS 30
+#define IH_EPSILON  1.0e-10
+
+/* region boundaries */
+#ifdef IT_WORKED
+#define IH_phi1  0.710987989993 
+#define IH_phi2  0.0            
+#define IH_phi3 -0.710987989993 
+#define IH_lam1 -1.74532925199  
+#define IH_lam2 -0.349065850399 
+#define IH_lam3  1.3962634016   
+#define IH_lam4 -0.698131700798 
+#endif
+
+/* 40N44'11.8" */
+#define IH_phi1  0.710987989993
+/* 0           */
+#define IH_phi2  0.0           
+/* 40S44'11.8" */
+#define IH_phi3 -0.710987989993
+/* 100W        */
+#define IH_lam1 -1.74532925199 
+/* 20W         */
+#define IH_lam2 -0.349065850399
+/* 80E         */ 
+#define IH_lam3  1.3962634016  
+/* 40W         */
+#define IH_lam4 -0.698131700798
+
+/* mystery constants */
+#define IH_mc1 0.900316316158
+#define IH_mc2 1.4142135623731
+#define IH_mc3 0.0528035274542
+
+#define IN_BREAK 1
+
+/* central meridians for each of the 12 regions
+  -------------------------------------------------------*/
+static const double IH_lam0[] = 
+{-1.74532925199,	/*  0 = -100.0 degrees */
+ -1.74532925199,	/*  1 = -100.0 degrees */
+  0.523598775598,	/*  2 = 30.0 degrees */
+  0.523598775598,	/*  3 = 30.0 degrees */
+ -2.79252680319,	/*  4 = -160.0 degrees */
+ -1.0471975512,		/*  5 = -60.0 degrees */
+ -2.79252680319,       	/*  6 = -160.0 degrees */
+ -1.0471975512,		/*  7 = -60.0 degrees */
+  0.349065850399,	/*  8 = 20.0 degrees */
+  2.44346095279,       	/*  9 = 140.0 degrees */
+  0.349065850399,	/* 10 = 20.0 degrees */
+  2.44346095279  	/* 11 = 140.0 degrees */
+};
+
+static int init_interupted_homolosine_equal_area(void)
+{
+  /* no variables require initialization */
+  return 0;
+}
+
+static int interupted_homolosine_equal_area(float lat, float lon, float *u, float *v)
+{
+  int it, region;
+  double x, y, phi, lam, cos_phi, sin_phi, delta_lam;
+  double feast, theta, delta_theta, constant;
+  
+  lam = RADIANS(lon);
+  phi = RADIANS(lat);
+  cos_phi = cos(phi);
+  sin_phi = sin(phi);
+  
+  if (phi >= IH_phi1)	             /* if on or above 40 44' 11.8" */
+  {
+    if (lam <= IH_lam4) region = 0;   /* If to the left of -40 */
+    else region = 2;
+  }
+  else if (phi >= IH_phi2)			     /* Between 0.0 and 40 44' 11.8" */
+  {
+    if (lam <= IH_lam4) region = 1;   /* If to the left of -40 */
+    else region = 3;
+  }
+  else if (phi >= IH_phi3)   	     /* Between 0.0 & -40 44' 11.8" */
+  {
+    if (lam <= IH_lam1) region = 4;  	/* If between -180 and -100 */
+    else if (lam <= IH_lam2) region = 5;/* If between -100 and -20 */
+    else if (lam <= IH_lam3) region = 8;	/* If between -20 and 80 */
+    else region = 9;				/* If between 80 and 180 */
+  }
+  else						/* Below -40 44' */
+  {
+    if (lam <= IH_lam1) region = 6;       /* If between -180 and -100 */
+    else if (lam <= IH_lam2) region = 7; /* If between -100 and -20 */
+    else if (lam <= IH_lam3) region = 10;   /* If between -20 and 80 */
+    else region = 11;                            /* If between 80 and 180 */
+  }
+
+  delta_lam = lam - IH_lam0[region];
+  RNORMALIZE(delta_lam);
+  feast = current->Rg * IH_lam0[region];
+ 
+  if (region==1||region==3||region==4||region==5||region==8||region==9)
+  {
+    x = feast + current->Rg * delta_lam * cos_phi;
+    y = current->Rg * phi;
+  }
+  else
+  {
+    theta = phi;
+    constant = PI*sin_phi;
+
+/* 
+ *	iterate using the Newton-Raphson method to find theta
+ */
+    for (it=0; it < IH_MAXITS; it++)
+    {
+      delta_theta = -(theta + sin(theta) - constant) / (1.0 + cos(theta));
+      theta += delta_theta;
+      if (fabs(delta_theta) < IH_EPSILON) break;
+    }
+
+    if (it >= IH_MAXITS) 
+    {
+#ifdef DEBUG
+      fprintf(stderr,"mapx: iteration failed to converge\n");
+#endif
+      return -1;
+    }
+    theta /= 2.0;
+    x = feast + IH_mc1 * current->Rg * delta_lam * cos(theta);
+    y = current->Rg * (IH_mc2*sin(theta) - IH_mc3*sign(phi));
+  }
+
+  *u = current->T00*x + current->T01*y - current->u0;
+  *v = current->T10*x + current->T11*y - current->v0;
+
+  return 0;
+}
+
+static int inverse_interupted_homolosine_equal_area(float u, float v, float *lat, float *lon)
+{
+  int region;
+  double x, y, xor, yor, lam, phi, theta, arg, feast; 
+  
+  x =  current->T00*(u+current->u0) - current->T01*(v+current->v0);
+  y = -current->T10*(u+current->u0) + current->T11*(v+current->v0);
+
+  xor = x/current->Rg;
+  yor = y/current->Rg;
+
+/*
+ *	get region
+ */
+  if (yor >= IH_phi1)  /* if on or above 40 44' 11.8" */
+  {
+    if (xor <= IH_lam4) region = 0; /* If to the left of -40 */
+    else region = 2;
+  }
+  else if (yor >= IH_phi2)                      /* Between 0.0 and 40 44' 11.8" */
+  {
+    if (xor <= IH_lam4) region = 1; /* If to the left of -40 */
+    else region = 3;
+  }
+  else if (yor >= IH_phi3)           /* Between 0.0 & -40 44' 11.8" */
+  {
+    if (xor <= IH_lam1) region = 4;     /* If between -180 and -100 */
+    else if (xor <= IH_lam2) region = 5; /* If between -100 and -20 */
+    else if (xor <= IH_lam3) region = 8;  /* If between -20 and 80 */
+    else region = 9;                             /* If between 80 and 180 */
+  }
+  else                                            /* Below -40 44' 11.8" */
+  {
+    if (xor <= IH_lam1) region = 6;     /* If between -180 and -100 */
+    else if (xor <= IH_lam2) region = 7; /* If between -100 and -20 */
+    else if (xor <= IH_lam3) region = 10; /* If between -20 and 80 */
+    else region = 11;                            /* If between 80 and 180 */
+  }
+
+  feast = current->Rg * IH_lam0[region];
+  x -= feast;
+
+  if (region==1||region==3||region==4||region==5||region==8||region==9)
+  {
+    phi = y / current->Rg;
+    if (fabs(phi) > PI/2) return -1;
+
+    if (fabs(fabs(phi) - PI/2) > IH_EPSILON)
+    {
+      lam = IH_lam0[region] + x / (current->Rg * cos(phi));
+      RNORMALIZE(lam);
+    }
+    else lam = IH_lam0[region];
+  }
+  else
+  {
+    arg = (y + IH_mc3*current->Rg * sign(y)) / (IH_mc2*current->Rg);
+    if (fabs(arg) > 1.0) return -1;
+    theta = asin(arg);
+    lam = IH_lam0[region] + x/(IH_mc1*current->Rg*cos(theta));
+    if (lam < -PI) return -1;
+    arg = (2*theta + sin(2*theta)) / PI;
+    if (fabs(arg) > 1.0) return -1;
+    phi = asin(arg);
+  }
+
+/*
+ * check for interupted area
+ */
+  switch (region)
+  { case 0: case 1: if (lam < -PI || lam > IH_lam4) return -1; break;
+    case 2: case 3: if (lam < IH_lam4 || lam > PI) return -1; break;
+    case 4: case 6: if (lam < -PI || lam > IH_lam1) return -1; break;
+    case 5: case 7: if (lam < IH_lam1 || lam > IH_lam2) return -1; break;
+    case 8: case 10: if (lam < IH_lam2 || lam > IH_lam3) return -1; break;
+    case 9: case 11: if (lam < IH_lam3 || lam > PI) return -1; break;
+  }
+
+  *lat = DEGREES(phi);
+  *lon = DEGREES(lam);
+  NORMALIZE(*lon);
+  
+  return 0;
+}
 
 #ifdef MTEST
 /*------------------------------------------------------------------------
@@ -1700,8 +1946,7 @@ main(int argc, char *argv[])
     
     printf("\ninverse_mapx:\n");
     for (;;)
-    { int temp;
-      printf("enter u v - ");
+    { printf("enter u v - ");
       gets(readln);
       if (feof(stdin)) { printf("\n"); exit(0);}
       if (*readln == '\0') break;
