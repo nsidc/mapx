b6e24651a6f53c4f0b29a7278bbb122619c27327
Ken Knowles <programmers@nsidc.org>
1998-06-15 16:24:23 +0000

    *** empty log message ***

--- grid_io.c
+++ grid_io.c
@@ -4,13 +4,15 @@
  * 3/18/98 K.Knowles knowles@kryos.colorado.edu 303-492-0644
  * National Snow & Ice Data Center, University of Colorado, Boulder
  *======================================================================*/
-static const char grid_io_c_RCSID[]="$Header: /tmp_mnt/FILES/mapx/grid_io.c,v 1.2 1998-06-11 22:31:25 knowles Exp $";
+static const char grid_io_c_RCSID[]="$Header: /tmp_mnt/FILES/mapx/grid_io.c,v 1.3 1998-06-15 16:24:23 knowles Exp $";
 
 #include "define.h"
 #include "matrix.h"
 #include "grid_io.h"
 
-#define MAX_ROW_BUFFER_SIZE (256*1024)
+#ifndef MAX_ROW_BUFFER_SIZE
+#define MAX_ROW_BUFFER_SIZE (16*1024*1024)
+#endif
 
 #ifdef DEBUG
 static int x_rdpix=-1, y_rdpix=-1, test_rdpix=0;
