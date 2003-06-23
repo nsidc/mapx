5e3fbc6108b078bd48612eb3d18486307a7ced9b
Terry Haran <tharan@nsidc.org>
2003-06-23 15:38:24 +0000

    7) The following changes were made to all projection .c files:    a) For forward projection functions, the output parameters *u and *v were       changed to *x and *y, respectively, and the following two lines of       code: 	*u = current->T00*x + current->T01*y - current->u0; 	*v = current->T10*x + current->T11*y - current->v0;       were replaced with: 	*x += current->false_easting; 	*y += current->false_northing;       All input and output parameters were changed from (float) and (float       *) to (double) and (double *), respectively.    b) For inverse projection functions, the input parameters u and v were       changed to x and y, respectively, and the following two lines of       code:         x =  current->T00*(u+current->u0) - current->T01*(v + current->v0);         y = -current->T10*(u+current->u0) + current->T11*(v + current->v0);       were replaced with         x -= current->false_easting;         y -= current->false_northing;       All input and output parameters were changed from (float) and (float       *) to (double) and (double *), respectively.
    8) albers_conic_equal_area.c:
       a) Added include of proj.h for use of asinz() in place of asin().
       b) In inverse_albers_conic_equal_area() and
          albers_conic_equal_area_ellipsoid(), in the calculation of theta,
          the arguments to atan2() are now negated when n is negative as
          specified in Snyder.
       c) In inverse_albers_conic_equal_area_ellipsoid(), the series equation
          3-18 from Snyder was replaced with the iterative solution based on
          equation 3-16 in Snyder. This, together with the float to double
          changes mentioned above, improved the overall accuracy by about
          a factor of about 2500:
          i)   macct sgi_albers_conic_equal_area_e00.gpd
                 Old:
                   102400 points,  336 bad points
                   average error = 3.6677e-02 km
                   std dev error = 9.4522e-02 km
                   maximum error = 4.4933e+00 km
                   max error was at 90.00S 124.70E
    	     New:
                   102400 points,  0 bad points
                   average error = 1.3655e-05 km
                   std dev error = 3.4695e-05 km
                   maximum error = 1.3426e-04 km
                   max error was at 73.07S 96.49E

--- albers_conic_equal_area.c
+++ albers_conic_equal_area.c
@@ -1,8 +1,16 @@
 /*------------------------------------------------------------------------
  * albers_conic_equal_area
  *------------------------------------------------------------------------*/
+static const char albers_conic_equal_area_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/albers_conic_equal_area.c,v 1.3 2003-06-23 15:38:24 haran Exp $";
+
+#include "proj.h"
 #include "define.h"
 #include "mapx.h"
+
+char *id_albers_conic_equal_area(void)
+{
+  return((char *)albers_conic_equal_area_c_rcsid);
+}
 
 int init_albers_conic_equal_area(mapx_class *current)
 {
@@ -24,10 +32,9 @@ int init_albers_conic_equal_area(mapx_class *current)
   return 0;
 }
 
-int albers_conic_equal_area(mapx_class *current, float lat, float lon, 
-			    float *u, float *v)
+int albers_conic_equal_area(mapx_class *current, double lat, double lon, 
+			    double *x, double *y)
 {
-  float x, y;
   double phi, lam, sin_phi, rho, theta;
   
   phi = RADIANS(lat);
@@ -37,26 +44,28 @@ int albers_conic_equal_area(mapx_class *current, float lat, float lon,
   rho = current->Rg*sqrt(current->C - 2*current->n*sin_phi)/current->n;
   theta = current->n*lam;
 
-  x = rho*sin(theta);
-  y = current->rho0 - rho*cos(theta);
+  *x = rho*sin(theta);
+  *y = current->rho0 - rho*cos(theta);
 
-  *u = current->T00*x + current->T01*y - current->u0;
-  *v = current->T10*x + current->T11*y - current->v0;
+  *x += current->false_easting;
+  *y += current->false_northing;
   
   return 0;
 }
 
-int inverse_albers_conic_equal_area(mapx_class *current, float u, float v, 
-				    float *lat, float *lon)
+int inverse_albers_conic_equal_area(mapx_class *current, double x, double y, 
+				    double *lat, double *lon)
 {
-  double phi, lam, rho, rmy, theta, chi, x, y;
+  double phi, lam, rho, rmy, theta, chi;
 
-  x =  current->T00*(u+current->u0) - current->T01*(v + current->v0);
-  y = -current->T10*(u+current->u0) + current->T11*(v + current->v0);
+  x -= current->false_easting;
+  y -= current->false_northing;
 
   rmy = current->rho0 - y;
   rho = sqrt(x*x + rmy*rmy);
-  theta = atan2(x, rmy);
+  theta = current->n >= 0 ?
+    atan2( x,  rmy) :
+    atan2(-x, -rmy);
 
   chi = rho*current->n/current->Rg;
   phi = asin((current->C - chi*chi)/(2*current->n));
@@ -133,11 +142,11 @@ int init_albers_conic_equal_area_ellipsoid(mapx_class *current)
 }
 
 
-int albers_conic_equal_area_ellipsoid(mapx_class *current, float lat, float lon, 
-			    float *u, float *v)
+int albers_conic_equal_area_ellipsoid(mapx_class *current,
+				      double lat, double lon, 
+				      double *x, double *y)
 {
 
-  float x, y;
   double phi, lam, sin_phi, rho, theta, q;
   
   phi = RADIANS(lat);
@@ -158,50 +167,78 @@ int albers_conic_equal_area_ellipsoid(mapx_class *current, float lat, float lon,
   
   theta = current->n * lam;
   
-  x = rho * sin(theta);
-  y = current->rho0 - (rho * cos(theta));
+  *x = rho * sin(theta);
+  *y = current->rho0 - (rho * cos(theta));
   
-  *u = current->T00*x + current->T01*y - current->u0;
-  *v = current->T10*x + current->T11*y - current->v0;
-
+  *x += current->false_easting;
+  *y += current->false_northing;
+  
   return 0;
 }
 
 
-int inverse_albers_conic_equal_area_ellipsoid(mapx_class *current, float u, float v, 
-				    float *lat, float *lon)
+int inverse_albers_conic_equal_area_ellipsoid(mapx_class *current,
+					      double x, double y, 
+					      double *lat, double *lon)
 {
-  double phi, lam, rho, rmy, theta, x, y, q, beta, sin_2beta,
-         sin_4beta, sin_6beta;       
+  double phi, lam, rho, rmy, theta, q;
+  double q_test;
+  double cos_phi;
+  double sin_phi;
+  double esin_phi;
+  double one_m_e2sin2_phi;
+  double delta_phi;
+  double one_m_e2;
+  double one_over_2e;
+  double epsilon = 1e-6;
+  int it_max = 35;
+  int i;
   
-  x =  current->T00*(u+current->u0) - current->T01*(v + current->v0);
-  y = -current->T10*(u+current->u0) + current->T11*(v + current->v0);
+  x -= current->false_easting;
+  y -= current->false_northing;
 
   rmy = current->rho0 - y;
   rho = sqrt( x*x + rmy*rmy );
   
-  theta = atan2(x,rmy);
+  theta = current->n >= 0 ?
+    atan2( x,  rmy) :
+    atan2(-x, -rmy);
+  lam = theta / current->n;
   
-  q = (current->C - ( (rho*rho*current->n*current->n) / (current->Rg*current->Rg) ) ) /
-          current->n;
+  
+  q = (current->C - ( (rho*rho*current->n*current->n) /
+		      (current->Rg*current->Rg) ) ) / current->n;
           
-          
-/***  Calculate phi using equation 3-18, Snyder 1987, p102 ***/
+/***  Calculate phi using equation 3-16, Snyder 1987, p102 ***/
  
-   beta = asin( q / (1 - ( ((1-current->e2)/(2*current->eccentricity)) *
-                  log( (1-current->eccentricity)/(1+current->eccentricity) ) )) );
-                   
-   sin_2beta = sin(2*beta);
-   sin_4beta = sin(4*beta);
-   sin_6beta = sin(6*beta);
+  q_test = 1.0 - (1.0 - current->e2)/(2.0 * current->eccentricity) *
+    log((1.0 - current->eccentricity) / (1.0 + current->eccentricity));
+  if (fabs(fabs(q) - fabs(q_test)) < epsilon) {
+    phi = sign(q) * PI / 2;
+  } else {
+
+    phi = asinz(q / 2.0);
+    one_m_e2 = 1.0 - current->e2;
+    one_over_2e = 1.0 / (2.0 * current->eccentricity);
     
-   phi = beta + ( ((current->e2/3 + ((517/5040) * current->e6))*sin_2beta) +
-             ((((23/360)*current->e4) + ((251/3780)*current->e6))*sin_4beta) +
-             (((761/3780)*current->e6)*sin_6beta) );
- 
-  
-   lam = theta / current->n;
-  
+    for (i = 0; i < it_max; i++) {
+      cos_phi = cos(phi);
+      if (cos_phi < epsilon) {
+	phi = sign(q) * PI / 2;
+	break;
+      }
+      sin_phi = sin(phi);
+      esin_phi = current->eccentricity * sin_phi;
+      one_m_e2sin2_phi = 1.0 - esin_phi * esin_phi;
+      delta_phi = one_m_e2sin2_phi * one_m_e2sin2_phi / (2.0 * cos_phi) *
+	(q / one_m_e2 - sin_phi / one_m_e2sin2_phi + one_over_2e *
+	 log((1.0 - esin_phi) / (1.0 + esin_phi)));
+      phi += delta_phi;
+      if (fabs(delta_phi) < epsilon)
+	break;
+    }
+  }
+
   *lat = DEGREES(phi);
   *lon = DEGREES(lam) + current->lon0;
   NORMALIZE(*lon);
