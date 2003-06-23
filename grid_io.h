dbabcdff045c8c441a76d6d24a681469490acbbe
Terry Haran <tharan@nsidc.org>
2003-06-23 16:00:57 +0000

    2) Any <module>_RCSID identifies were changed to <module>_rcsid.

--- grid_io.h
+++ grid_io.h
@@ -9,7 +9,7 @@
 
 #include "define.h"
 
-static const char grid_io_h_RCSID[]="$Header: /tmp_mnt/FILES/mapx/grid_io.h,v 1.3 1999-08-31 20:29:17 knowles Exp $";
+static const char grid_io_h_rcsid[]="$Header: /tmp_mnt/FILES/mapx/grid_io.h,v 1.4 2003-06-23 16:00:57 haran Exp $";
 
 typedef enum 
 { grid_io_READ_ONLY, 
