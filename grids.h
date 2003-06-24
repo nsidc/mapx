e79ffe4cc1ac2777df65b76eb7b6c3baf6ecfc6a
Terry Haran <tharan@nsidc.org>
2003-06-24 22:03:55 +0000

    21) grids.h:     a) Changed float to double throughout.

--- grids.h
+++ grids.h
@@ -7,7 +7,7 @@
 #ifndef grids_h_
 #define grids_h_
 
-static const char grids_h_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/grids.h,v 1.7 1999-11-19 16:56:49 knowles Exp $";
+static const char grids_h_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/grids.h,v 1.8 2003-06-24 22:03:55 haran Exp $";
 
 #include "mapx.h"
 
@@ -32,8 +32,8 @@ GLOBAL int grid_verbose;
  * grid parameters structure
  */
 typedef struct {
-	float map_origin_col, map_origin_row;
-	float cols_per_map_unit, rows_per_map_unit;
+	double map_origin_col, map_origin_row;
+	double cols_per_map_unit, rows_per_map_unit;
 	int cols, rows;
 	FILE *gpd_file;
 	char *gpd_filename;
@@ -46,7 +46,9 @@ typedef struct {
 grid_class *init_grid(char *filename);
 grid_class *new_grid(char *label);
 void close_grid(grid_class *this);
-int forward_grid(grid_class *this, float lat, float lon, float *r, float *s);
-int inverse_grid(grid_class *this, float r, float s, float *lat, float *lon);
+int forward_grid(grid_class *this,
+		 double lat, double lon, double *r, double *s);
+int inverse_grid(grid_class *this,
+		 double r, double s, double *lat, double *lon);
 
 #endif
