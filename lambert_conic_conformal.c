f6b023f649461f47872e8f225ef95101b7f10cda
Terry Haran <tharan@nsidc.org>
2003-06-24 22:37:30 +0000

    1) <module>_rcsid character strings were added where missing. 3) The function id_<module>() was added to all .c files. This function    returns a pointer to the rcsid string. This was done in order to    suppress warnings as well as provide a means for applications to    retrieve version information. 7) The following changes were made to all projection .c files:    a) For forward projection functions, the output parameters *u and *v were       changed to *x and *y, respectively, and the following two lines of       code: 	*u = current->T00*x + current->T01*y - current->u0; 	*v = current->T10*x + current->T11*y - current->v0;       were replaced with: 	*x += current->false_easting; 	*y += current->false_northing;       All input and output parameters were changed from (float) and (float       *) to (double) and (double *), respectively.    b) For inverse projection functions, the input parameters u and v were       changed to x and y, respectively, and the following two lines of       code:         x =  current->T00*(u+current->u0) - current->T01*(v + current->v0);         y = -current->T10*(u+current->u0) + current->T11*(v + current->v0);       were replaced with         x -= current->false_easting;         y -= current->false_northing;       All input and output parameters were changed from (float) and (float       *) to (double) and (double *), respectively. 30) lambert_conic_conformal.c:     a) In init_lambert_conic_conformal_ellipsoid(), added use of Map        Origin Latitude (center_lat) in addition to Map Reference Latitude        (lat0) and Map Second Reference Latitude (lat1) as is done in        init_albers_conic_equal_area().     b) In inverse_lambert_conic_conformal_ellipsoid(), replaced lower        accuracy series equation 3-5 from Snyder with higher accuracy        iteration involving equation 7-9.

--- lambert_conic_conformal.c
+++ lambert_conic_conformal.c
@@ -1,38 +1,61 @@
 /*--------------------------------------------------------------------------
  * Lambert conic conformal ellipsoid
  *--------------------------------------------------------------------------*/
+static const char lambert_conic_conformal_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/lambert_conic_conformal.c,v 1.3 2003-06-24 22:37:30 haran Exp $";
+
 #include "define.h"
 #include "mapx.h"
+
+char *id_lambert_conic_conformal(void)
+{
+  return((char *)lambert_conic_conformal_c_rcsid);
+}
 
 int init_lambert_conic_conformal_ellipsoid(mapx_class *current)
 {
   current->Rg = current->equatorial_radius / current->scale;
-  current->cos_phi0 = cos(RADIANS(current->lat0));
-  current->cos_phi1 = cos(RADIANS(current->lat1));
-  current->sin_phi0 = sin(RADIANS(current->lat0));
-  current->sin_phi1 = sin(RADIANS(current->lat1));
-  current->m0 = ((current->cos_phi0)/sqrt(1 - (current->e2 * current->sin_phi0 
-					     * current->sin_phi0))); 
-  current->m1 = ((current->cos_phi1)/sqrt(1 - (current->e2 * current->sin_phi1 
-					     * current->sin_phi1)));
+  current->sin_phi0 = sin(RADIANS(current->center_lat));
+  current->sin_phi1 = sin(RADIANS(current->lat0));
+  current->sin_phi2 = sin(RADIANS(current->lat1));
+  current->cos_phi1 = cos(RADIANS(current->lat0));
+  current->cos_phi2 = cos(RADIANS(current->lat1));
+  
+  current->m1 = current->cos_phi1 / 
+      sqrt(1 - (current->e2 * current->sin_phi1 * current->sin_phi1));
+  
+  current->m2 = current->cos_phi2 / 
+      sqrt(1 - (current->e2 * current->sin_phi2 * current->sin_phi2));
+  
   current->t0 = sqrt( ((1.0 - current->sin_phi0)/(1.0 + current->sin_phi0)) * 
 		     pow(((1.0 + (current->eccentricity * current->sin_phi0))/
 			  (1.0 - (current->eccentricity * current->sin_phi0))),
-			 current->eccentricity) ); 
+			 current->eccentricity) );
+ 
   current->t1 = sqrt( ((1.0 - current->sin_phi1)/(1.0 + current->sin_phi1)) * 
 		     pow(((1.0 +(current->eccentricity * current->sin_phi1))/
 			  (1.0 - (current->eccentricity * current->sin_phi1))),
-			 current->eccentricity) ); 
-  current->n = (log(current->m0) - log(current->m1)) / (log(current->t0) -log(current->t1));
-  current->F = current->m0/(current->n * pow(current->t0,current->n));
+			 current->eccentricity) );
+ 
+  current->t2 = sqrt( ((1.0 - current->sin_phi2)/(1.0 + current->sin_phi2)) * 
+		     pow(((1.0 +(current->eccentricity * current->sin_phi2))/
+			  (1.0 - (current->eccentricity * current->sin_phi2))),
+			 current->eccentricity) );
+ 
+  current->n = ((log(current->m1) - log(current->m2)) /
+		(log(current->t1) - log(current->t2)));
+
+  current->F = current->m1 / (current->n * pow(current->t1, current->n));
+
   current->rho0 = current->Rg * current->F * pow(current->t0, current->n);
   
   return 0;
 }
 
-int lambert_conic_conformal_ellipsoid (mapx_class *current, float lat, float lon, float *u, float *v)
+int lambert_conic_conformal_ellipsoid (mapx_class *current,
+				       double lat, double lon,
+				       double *x, double *y)
 {
-  double phi, lam, x, y, rho, theta, sin_phi, t;
+  double phi, lam, rho, theta, sin_phi, t;
   
   lam = lon - current->lon0;
   NORMALIZE (lam);
@@ -47,42 +70,57 @@ int lambert_conic_conformal_ellipsoid (mapx_class *current, float lat, float lon
   rho = current->Rg * current->F * pow(t, current->n);
   theta = current->n * lam;
   
-  x = rho * sin(theta);
-  y = current->rho0 - (rho * cos(theta));
+  *x = rho * sin(theta);
+  *y = current->rho0 - (rho * cos(theta));
   
-  *u = current->T00*x + current->T01*y - current->u0;
-  *v = current->T10*x + current->T11*y - current->v0;
+  *x += current->false_easting;
+  *y += current->false_northing;
   
   return 0;
 }
 
-int inverse_lambert_conic_conformal_ellipsoid (mapx_class *current, float u, float v, float *lat, float *lon)
+int inverse_lambert_conic_conformal_ellipsoid (mapx_class *current,
+					       double x, double y,
+					       double *lat, double *lon)
 {
-  double rho, x, y, t, chi, lam, phi, theta; 
+  double rho, t, lam, phi, theta;
+  double rho0_m_y;
+  double esin_phi;
+  double e_over_2;
+  double phi_old;
+  double delta_phi;
+  double epsilon = 1e-7;
+  int it_max = 35;
+  int i;
   
-  x =  current->T00*(u+current->u0) - current->T01*(v+current->v0);
-  y = -current->T10*(u+current->u0) + current->T11*(v+current->v0);
-  
-  rho = (fabs(current->n) / current->n) * 
-    sqrt((x*x) + ((current->rho0 - y) * (current->rho0 - y)));
-  t = pow((rho/(current->Rg * current->F)), (1/current->n));
-  chi = PI/2.0 - 2.0 * atan(t);
-  
-  if (current->n < 0.0)
-    theta = atan( -x / (y - current->rho0));
-  else
-    theta = atan(x / (current->rho0 - y));
-  
+  x -= current->false_easting;
+  y -= current->false_northing;
+
+  rho0_m_y = current->rho0 - y;
+  rho = sign(current->n) * sqrt(x * x + rho0_m_y * rho0_m_y);
+  theta = current->n >= 0 ?
+    atan2( x,  rho0_m_y) :
+    atan2(-x, -rho0_m_y);
   lam = (theta / current->n);
-  phi = chi + (((current->e2 / 2.0) + ((5.0 / 24.0) * current->e4) +
-		(current->e6 / 12.0) + ((13.0 / 360.0) * current->e8)) * 
-	       sin(2.0 * chi)) +
-		 ((((7.0 / 48.0) * current->e4) + ((29.0 / 240.0) * current->e6) +
-		   ((811.0 / 11520.0) * current->e8)) * sin(6.0 * chi)) +
-		     ((((7.0 / 120.0) * current->e6) + 
-		       ((81.0 / 1120.0) * current->e8)) * sin(6.0 * chi)) +
-			 (((4279.0 / 161280.0) * current->e8) * sin(8.0 * chi));
-  
+
+  t = pow((rho/(current->Rg * current->F)), (1/current->n));
+  e_over_2 = current->eccentricity / 2.0;
+  phi = PI / 2.0 - 2 * atan(t);
+  phi_old = phi;
+
+  /*
+   * Calculate phi using equation 7-9, Snyder 1987, p109
+   */
+
+  for (i = 0; i < it_max; i++) {
+    esin_phi = current->eccentricity * sin(phi);
+    phi = PI / 2.0 - 2.0 * atan(t * pow((1 - esin_phi) / (1 + esin_phi),
+					e_over_2));
+    delta_phi = fabs(fabs(phi) - fabs(phi_old));
+    if (delta_phi < epsilon)
+      break;
+    phi_old = phi;
+  }
   
   *lat = DEGREES(phi);
   *lon = DEGREES(lam) + current->lon0;
