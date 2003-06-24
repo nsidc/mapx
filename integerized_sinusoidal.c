154cae71ac6e858f38d69184f27e3e381a583a7a
Terry Haran <tharan@nsidc.org>
2003-06-24 22:08:02 +0000

    1) <module>_rcsid character strings were added where missing. 3) The function id_<module>() was added to all .c files. This function    returns a pointer to the rcsid string. This was done in order to    suppress warnings as well as provide a means for applications to    retrieve version information.
    7) The following changes were made to all projection .c files:
       a) For forward projection functions, the output parameters *u and *v were
          changed to *x and *y, respectively, and the following two lines of
          code:
    	*u = current->T00*x + current->T01*y - current->u0;
    	*v = current->T10*x + current->T11*y - current->v0;
          were replaced with:
    	*x += current->false_easting;
    	*y += current->false_northing;
          All input and output parameters were changed from (float) and (float
          *) to (double) and (double *), respectively.
       b) For inverse projection functions, the input parameters u and v were
          changed to x and y, respectively, and the following two lines of
          code:
            x =  current->T00*(u+current->u0) - current->T01*(v + current->v0);
            y = -current->T10*(u+current->u0) + current->T11*(v + current->v0);
          were replaced with
            x -= current->false_easting;
            y -= current->false_northing;
          All input and output parameters were changed from (float) and (float
          *) to (double) and (double *), respectively.
    22) integerized_sinusoidal.c:
        a) In init_integerized_sinusoidal(), replaced calculation of nrows
           with use of Map ISin NZone (isin_nzone) parameter value.
        b) In init_integerized_sinusoidal(), added passing of false easting
           and false northing values to Isin_inv_init().

--- integerized_sinusoidal.c
+++ integerized_sinusoidal.c
@@ -1,52 +1,57 @@
 /*------------------------------------------------------------------------
  * integerized sinusoidal
  *------------------------------------------------------------------------*/
+static const char integerized_sinusoidal_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/integerized_sinusoidal.c,v 1.2 2003-06-24 22:08:02 haran Exp $";
+
+#include "isin.h"
 #include "define.h"
 #include "mapx.h"
-#include "isin.h"
+
+char *id_integerized_sinusoidal(void)
+{
+  return((char *)integerized_sinusoidal_c_rcsid);
+}
 
 int init_integerized_sinusoidal(mapx_class *current)
-{ 
-  long nrows;
-
-  nrows = nint(PI*current->Rg);
-
+{
   current->isin_data = 
     (void *)Isin_inv_init(current->equatorial_radius,
 			  RADIANS(current->lon0), 
-			  (double)0, (double)0, 
-			  nrows, current->isin_justify);
+			  current->false_easting, current->false_northing, 
+			  current->isin_nzone, current->isin_justify);
 
   if (NULL == current->isin_data) return -1;
 
   return 0;
 }
 
-int integerized_sinusoidal(mapx_class *current, float lat, float lon, float *u, float *v)
+int integerized_sinusoidal(mapx_class *current,
+			   double lat, double lon, double *x, double *y)
 {
   int status;
-  double x, y, phi, lam;
+  double phi, lam;
   
   NORMALIZE(lon);
   
   phi = RADIANS(lat);
   lam = RADIANS(lon);
 
-  status = Isin_fwd((Isin_t *)(current->isin_data), lam, phi, &x, &y);
+  status = Isin_fwd((Isin_t *)(current->isin_data), lam, phi, x, y);
   
-  *u = current->T00*x + current->T01*y - current->u0;
-  *v = current->T10*x + current->T11*y - current->v0;
+  *x += current->false_easting;
+  *y += current->false_northing;
   
   return status;
 }
 
-int inverse_integerized_sinusoidal(mapx_class *current, float u, float v, float *lat, float *lon)
+int inverse_integerized_sinusoidal(mapx_class *current,
+				   double x, double y, double *lat, double *lon)
 {
   int status;
-  double phi, lam, x, y;
+  double phi, lam;
   
-  x =  current->T00*(u+current->u0) - current->T01*(v+current->v0);
-  y = -current->T10*(u+current->u0) + current->T11*(v+current->v0);
+  x -= current->false_easting;
+  y -= current->false_northing;
 
   status = Isin_inv((Isin_t *)(current->isin_data), x, y, &lam, &phi);
 
