70fad2ef5e66baabf92276320baadcd50b9bed85
Terry Haran <tharan@nsidc.org>
2003-06-24 22:34:27 +0000

    3) The function id_<module>() was added to all .c files. This function    returns a pointer to the rcsid string. This was done in order to    suppress warnings as well as provide a means for applications to    retrieve version information. 28) keyval.c:     a) In get_label_keyval(), added feature such that for each line,        any characters following a semi-colon or a pound sign are replaced        with blanks.     b) In get_field_keyval(), added feature such that keyword and label        being searched are both converted to uppercase before the search        is performed, and that a valid keyword must be followed immediately        by a colon.     c) In get_value_keyval(), modified behavior such that for format        strings "%lat" and "%lon", the value field returned by        lat_lon_keyval() is now parsed as a double rather than a float.     d) In get_value_keyval(), added feature such that if default_string is        "KEYVAL_UNINITIALIZED" and format is "%f" or "%lf", and the keyword        is not found, then the value KEYVAL_UNINITIALIZED is returned.     e) In lat_lon_keyval(), changed the value parameter from (float *) to        (double *).

--- keyval.c
+++ keyval.c
@@ -4,11 +4,17 @@
  * 23-Oct-1996 K.Knowles knowles@kryos.colorado.edu 303-492-0644
  * National Snow & Ice Data Center, University of Colorado, Boulder
  *======================================================================*/
-static const char keyval_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/keyval.c,v 1.6 2002-01-08 21:09:01 knowles Exp $";
+static const char keyval_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/keyval.c,v 1.7 2003-06-24 22:34:27 haran Exp $";
 
 #include <ctype.h>
 #include "define.h"
 #include "keyval.h"
+#include "mapx.h"
+
+char *id_keyval(void)
+{
+  return((char *)keyval_c_rcsid);
+}
 
 /*------------------------------------------------------------------------
  * get_label_keyval - read label from file
@@ -25,12 +31,17 @@ static const char keyval_c_rcsid[] = "$Header: /tmp_mnt/FILES/mapx/keyval.c,v 1.
  *
  * A "label" consists of a list of "keyword: value" pairs. The keyword
  * field is terminated by a colon and separated from the value field
- * by blanks or tabs. The value field is terminated by a semi-colon or 
- * newline. Each "keyword: value" pair describes a single parameter.
+ * by blanks or tabs. The value field is terminated by a semi-colon,
+ * pound sign or newline. Each "keyword: value" pair describes a single
+ * parameter.
+ * For each line, any characters following a semi-colon or pound sign
+ * are replaced with blanks.
  *
  *------------------------------------------------------------------------*/
 char *get_label_keyval(const char *filename, FILE *fp, int label_length)
 { char *label;
+ int i;
+ int got_comment;
 
   assert(fp != NULL || filename != NULL);
 
@@ -68,6 +79,20 @@ char *get_label_keyval(const char *filename, FILE *fp, int label_length)
 
   label[label_length] = '\0';
 
+  /*
+   *  For each line, replace any characters following a semi-colon
+   *  or a pound sign with blanks.
+   */
+  got_comment = 0;
+  for (i = 0; i < label_length; i++) {
+    if ('\n' == label[i])
+      got_comment = 0;
+    else if (';' == label[i] || '#' == label[i])
+      got_comment = 1;
+    else if (got_comment)
+      label[i] = ' ';
+  }
+
   return label;
 }
 
@@ -83,39 +108,98 @@ char *get_label_keyval(const char *filename, FILE *fp, int label_length)
  *		or NULL if field not found, space for buffer
  *		is obtained with malloc
  *
+ *      NOTE: keyword is converted to uppercase before the search is
+ *            performed.
+ *
  *------------------------------------------------------------------------*/
 char *get_field_keyval(const char *label, const char *keyword, 
 		       const char *default_string)
-{ register char *field_ptr, *field_start;
+{
+  register char *field_ptr, *field_start;
+  int i;
+  char *keyword_upper;
+  char *label_upper;
+  int keyword_length;
+  int label_length;
   int field_length;
+  int got_valid_keyword = 0;
+
+  /*
+   *  Convert keyword to uppercase
+   */
+
+  keyword_upper = strdup(keyword);
+  if (NULL == keyword_upper) { perror("get_field_keyval"); return NULL; }
+  keyword_length = strlen(keyword);
+  for (i = 0; i < keyword_length; i++)
+    keyword_upper[i] = toupper(keyword_upper[i]);
+
+  /*
+   *  Convert label to uppercase for searching for keyword
+   */
+
+  label_upper = strdup(label);
+  if (NULL == label_upper) { perror("get_field_keyval"); return NULL; }
+  label_length = strlen(label);
+  for (i = 0; i < label_length; i++)
+    label_upper[i] = toupper(label_upper[i]);
+  field_start = label_upper;
+
+  /*
+   *	find keyword
+   */
+  
+  while (!got_valid_keyword)
+    {
+
+    field_start = strstr(field_start, keyword_upper);
+
+    if (NULL == field_start)
+      { 
+	free(keyword_upper);
+	free(label_upper);
+	if (NULL == default_string)
+	  { fprintf(stderr,"get_field_keyval: <%s> not found\n", keyword);
+	  return NULL;
+	  }
+	else
+	  { return strdup(default_string);
+	  }
+      } else {
+
+	/*
+	 *  Make sure that the keyword is immediately followed by a colon
+	 */
+
+	field_start += keyword_length;
+	if (field_start - label_upper + 2 < label_length) {
+	  if (*field_start++ == ':') {
+
+	    /*
+	     *  Reset the field pointer to the same position in the
+	     *  original label. This will allow lower case in
+	     *  the field values (e.g. an mpp filename).
+	     */
+	    
+	    field_start = (char *)label + (field_start - label_upper);
+	    got_valid_keyword = 1;
+	  }
+	}
+      }
+    }
 
 /*
- *	find keyword
+ *	skip to start of value field
  */
-  field_start = strstr(label, keyword);
 
-  if (NULL == field_start)
-  { 
-    if (NULL == default_string)
-    { fprintf(stderr,"get_field_keyval: <%s> not found\n", keyword);
-      return NULL;
-    }
-    else
-    { return strdup(default_string);
-    }
-  }
-
-/*
- *	skip to end of keyword and start of value field
- */
-  field_start = strchr(field_start, (int)':') + 1;
-
-  field_start += strspn(field_start, " \t");
+  free(keyword_upper);
+  free(label_upper);
+  field_start += strspn(field_start, " \t\n");
 
 /*
  *	get length of field and allocate new buffer
  */
-  field_length = strcspn(field_start, ";\n");
+  field_length = strcspn(field_start, ";#\n");
   if (0 == field_length) field_length = strlen(field_start);
 
   field_ptr = (char *)malloc(field_length+1);
@@ -139,8 +223,10 @@ char *get_field_keyval(const char *label, const char *keyword,
  *			 "%lon" for longitude
  *			 "%bool" for boolean value (see below)
  *			 or regular scanf format string
- *		default_string - pointer to default string or NULL
- *			  if keyword is not found then default is returned
+ *		default_string - pointer to default string or NULL.
+ *			  If keyword is not found then default is returned.
+ *                        Use "KEYVAL_UNINITIALIZED" to return
+ *                        KEYVAL_UNINITIALIZED for %f or %lf.
  *
  *	output: value from header label field (or converted default value)
  *
@@ -164,14 +250,30 @@ bool get_value_keyval(const char *label, const char *keyword,
  */
   if (streq("%lat", format))
   { status = lat_lon_keyval(field_ptr, keyval_LATITUDE, value);
-    if (!status) status = sscanf(field_ptr, "%f", (float *)value);
+    if (!status) status = sscanf(field_ptr, "%lf", (double *)value);
   }
   else if (streq("%lon", format))
   { status = lat_lon_keyval(field_ptr, keyval_LONGITUDE, value);
-    if (!status) status = sscanf(field_ptr, "%f", (float *)value);
+    if (!status) status = sscanf(field_ptr, "%lf", (double *)value);
   }
   else if (streq("%bool", format))
   { status = boolean_keyval(field_ptr, value);
+  }
+  else if (streq("%f", format)) {
+    if (streq("KEYVAL_UNINITIALIZED", field_ptr)) {
+      *(float *)value = (float)KEYVAL_UNINITIALIZED;
+      status = 1;
+    } else {
+      status = sscanf(field_ptr, format, value);
+    }
+  }
+  else if (streq("%lf", format)) {
+    if (streq("KEYVAL_UNINITIALIZED", field_ptr)) {
+      *(double *)value = KEYVAL_UNINITIALIZED;
+      status = 1;
+    } else {
+      status = sscanf(field_ptr, format, value);
+    }
   }
   else
   { status = sscanf(field_ptr, format, value);
@@ -258,7 +360,7 @@ int boolean_keyval(const char *field_ptr, bool *value)
  *
  *------------------------------------------------------------------------*/
 int lat_lon_keyval(const char *field_ptr, const char *designators, 
-		   float *value)
+		   double *value)
 { const char *end, *pos;
   char hemi, number[MAX_STRING];
   int len;
@@ -290,7 +392,7 @@ int lat_lon_keyval(const char *field_ptr, const char *designators,
   strncpy(number, pos, len);
   number[len] = '\0';
 
-  if (sscanf(number, "%f", value) != 1) return 0;
+  if (sscanf(number, "%lf", value) != 1) return 0;
 
 /*
  *	change sign for "negative" hemispheres
